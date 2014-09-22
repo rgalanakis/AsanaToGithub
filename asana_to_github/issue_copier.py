c7455864a393108f653c66173faa67fd67854df4
Rob Galanakis <rob.galanakis@gmail.com>
2014-09-21 17:46:28 -0700

    Fixed issue copier bugs. Works real nice now.

--- asana_to_github/issue_copier.py
+++ asana_to_github/issue_copier.py
@@ -1,12 +1,13 @@
+from __future__ import print_function
 from dateutil import parser as dtparser
 
 
-def _parse_asana_stories(asana_api, task_id):
+def _parse_asana_stories(asana_api, task):
     """Read the Asana task's stories and return ````."""
     comments = []
     attachments = []
     created_by = ''
-    all_stories = asana_api.list_stories(task_id)
+    all_stories = asana_api.list_stories(task['id'])
     for astory in all_stories:
         if astory['type'] == 'comment':
             the_time = dtparser.parse(astory['created_at'])
@@ -18,18 +19,11 @@ def _parse_asana_stories(asana_api, task_id):
         if astory['type'] == 'system':
             if astory['text'][:9] == 'attached ':
                 attachments.append('1. [Link to attachment]({})'.format(astory['text'][9:]))
-            if astory['text'].startswith('added '):
-                assert not created_by, 'created_by should only be set once!'
+            # Take the first 'Added to <project>' comment to see who created it.
+            if astory['text'].startswith('added ') and not created_by:
                 created_by = astory['created_by']['name']
 
     return {'comments': comments, 'attachments': attachments, 'created_by': created_by}
-    # if comment:
-    #     comment = '### Comments\n' + comment
-    # if attachment:
-    #     attachment = '### Attachments\n' + attachment
-    # if comment or attachment:
-    #     final_comment = attachment + '\n' + comment
-    #     issue.create_comment(final_comment)
 
 
 def _parse_asana_task(asana_api, task):
@@ -50,17 +44,17 @@ def create_github_issue(asana_api, task, git_repo, options):
     print('Creating issue for {}: {}'.format(task['id'], task['name'].encode('utf-8')))
     
     task_data = _parse_asana_task(asana_api, task)
-    bodylines = task_data['description']
+    bodylines = [task_data['description']]
     if task_data['comments']:
-        bodylines.append('###Comments')
+        bodylines.append('\n### Comments\n')
         bodylines.extend(task_data['comments'])
     if task_data['attachments']:
-        bodylines.append('###Attachments')
+        bodylines.append('\n### Attachments\n')
         bodylines.extend(task_data['attachments'])
-    bodylines.append('###Meta')
+    bodylines.append('\n### Meta\n')
     bodylines.append('[Asana task]({url}) was created by {who} at {when}'.format(
-        url=task_data['url'], who=task_data['created_by'], when=task_data['created_by']))
-    body = bodylines.join('\n')
+        url=task_data['url'], who=task_data['created_by'], when=task_data['created_at']))
+    body = '\n'.join(bodylines)
     new_issue = git_repo.create_issue(task['name'], body)
     
     if not options.dont_update_story:
